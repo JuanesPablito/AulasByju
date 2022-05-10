2f757304f1117d3704dc06594d4b05555cff2d1e
JuanesPablito <105311057+JuanesPablito@users.noreply.github.com>
2022-05-10 15:01:24 -0300

    aprendi o github

---
+++ Aula 9/p5play_template-main/sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+ box.x+=5   
+  }
+drawSprites()
+}
+var box;
+
+
